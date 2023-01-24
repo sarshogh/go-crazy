875d079262ab89d8fb3c1ffb5eab4c6cc9e7a453
Harry <Ali.Sarshogh@outlook.com>
2023-01-24 11:23:05 +0000

    Refactoring isPrime and its test green-rafactor phase

--- algorithms/prime_test.go
+++ algorithms/prime_test.go
@@ -24,11 +24,9 @@ func Test_Seve_IsPrime(t *testing.T) {
 			subject := item.input
 			expected := item.output
 			result := IsPrime(subject)
+			if result != expected {
+				t.Errorf("got %t, want %t", result, expected)
 
-			if result == expected {
-				t.Logf("the subject with value of %d is a prime number", subject)
-			} else {
-				t.Errorf("the number %d is not a prime number", subject)
 			}
 		})
 	}
