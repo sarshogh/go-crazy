94826993656516c6c43ff989957abf8eb6834653
Harry <Ali.Sarshogh@outlook.com>
2023-01-24 11:15:21 +0000

    Refactoring isPrime and its test green-rafactor phase

--- algorithms/prime_test.go
+++ algorithms/prime_test.go
@@ -1,29 +1,35 @@
 package algorithms
 
 import (
+	"fmt"
 	"testing"
 )
 
-func Test_IsPrime(t *testing.T) {
-	subject := 5
-	expected := true
-	result := IsPrime(subject)
-
-	if result == expected {
-		t.Logf("the subject with value of %d is a prime number", subject)
-	} else {
-		t.Errorf("the number %d is not a prime number", subject)
-	}
-}
-func Test_IsNotPrime(t *testing.T) {
-	subject := 8
-	expected := false
-	result := IsPrime(subject)
-
-	if result == expected {
-		t.Logf("the subject with value of %d is a prime number", subject)
-	} else {
-		t.Errorf("the number %d is not a prime number", subject)
-	}
+// table-driven tests
+var sample_numbers = []struct {
+	name   string
+	input  int
+	output bool
+}{
+	{"isPrime(1)", 1, true},
+	{"isPrime(4)", 4, false},
+	{"isPrime(21)", 21, false},
+	{"isPrime(19)", 19, true},
 }
 
+func Test_Seve_IsPrime(t *testing.T) {
+	for index, item := range sample_numbers {
+		fmt.Printf("runnig IsPrime test case %d \n", index)
+		t.Run(item.name, func(t *testing.T) {
+			subject := item.input
+			expected := item.output
+			result := IsPrime(subject)
+
+			if result == expected {
+				t.Logf("the subject with value of %d is a prime number", subject)
+			} else {
+				t.Errorf("the number %d is not a prime number", subject)
+			}
+		})
+	}
+}
